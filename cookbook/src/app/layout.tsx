fc361287db85346350acb5ec9b8dd5e5db06f781
mariarintala <168300660+mariarintala@users.noreply.github.com>
2024-06-29 18:06:12 +0300

    metas

--- cookbook/src/app/layout.tsx
+++ cookbook/src/app/layout.tsx
@@ -1,10 +1,18 @@
-import Script from "next/script";
 import GoogleAnalytics from "@/components/GoogleAnalytics";
 import GoogleTagmanager from "@/components/GoogleAnalytics";
+import Head from "next/head";
+import type { Metadata } from "next";
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Cookbook demo",
   description: "Lorem ipsum dolor sit",
+  openGraph: {
+    title: "Cookbook",
+    description: "Lorem ipsum dolor sit",
+    images: [
+      "https://cdn.sanity.io/images/hm2w9yds/production/13a55b8e9ffbb2fa8dbe3bcf285ef9fb021966bc-770x513.jpg",
+    ],
+  },
 };
 
 export default function RootLayout({
